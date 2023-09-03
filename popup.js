3656afbfe4c1adb1846ac13539aacfe705c2404e
BarelyAnXer <christiangazzingan38@gmail.com>
2023-09-03 21:37:23 +0800

    trying trying imorting all events

--- popup.js
+++ popup.js
@@ -20,37 +20,30 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     // const output = separateSchedules(request.schedule);
     // const iCalData = jsonToICal(output);
 
-    let iCalData = `BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//Your Organization//Your Website//EN
-CALSCALE:GREGORIAN
-METHOD:PUBLISH`
-
-    // BEGIN:VEVENT
-    // UID:9876543210
-    // DTSTAMP:20230903T120000Z
-    // DTSTART:20230903T160000Z
-    // DTEND:20230903T180000Z
-    // SUMMARY:Random Event
-    // DESCRIPTION:This is a randomly generated event description.
-    // LOCATION:Virtual Meeting
-    // ORGANIZER:John Doe
-    // ATTENDEE:Jane Doe
-    // END:VEVENT
-    // END:VCALENDAR
-
-//     iCalData = iCalData + `BEGIN:VEVENT
-// UID:${uid}
-// SUMMARY:${event.subject}
-// DTSTART:${formatICalDate(startDate)}
-// DTEND:${formatICalDate(endDate)}
-// RRULE:FREQ=WEEKLY;UNTIL=${formatICalDate(endRecurringDate)}
-// END:VEVENT\n`;
+    //     let iCalData = `BEGIN:VCALENDAR
+    // VERSION:2.0
+    // PRODID:-//Your Organization//Your Website//EN
+    // CALSCALE:GREGORIAN
+    // METHOD:PUBLISH`
 
 
-    iCalData += 'END:VCALENDAR';
+    //     iCalData = iCalData + `BEGIN:VEVENT
+    // UID:${uid}
+    // SUMMARY:${event.subject}
+    // DTSTART:${formatICalDate(startDate)}
+    // DTEND:${formatICalDate(endDate)}
+    // RRULE:FREQ=WEEKLY;UNTIL=${formatICalDate(endRecurringDate)}
+    // END:VEVENT\n`;
 
-    downloadICS(iCalData, "schedule");
+
+    //     iCalData += 'END:VCALENDAR';
+
+
+
+
+    const icsContent = convertToICS(request.schedule);
+
+    downloadICS(icsContent, "schedule");
 
     if (!newTag) {
         const displayText = document.createElement("p");
@@ -76,12 +69,55 @@ METHOD:PUBLISH`
     // }
 });
 
-function scheduleToEvents(schedule) {
-
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
 }
 
-function constructICS() {
+function convertToICS(data) {
+    let icsData = "BEGIN:VCALENDAR\r\n";
+    icsData += "VERSION:2.0\r\n";
 
+    data.forEach(event => {
+        // pwede magka undefined dito
+        const summary = event.subjectName;
+        
+        event.dates.forEach(day => {
+            event.times.forEach(time => {
+                const uid = generateUUID();
+                const dtStart = `202309${getDayCode(day)}T${formatTime(time.starttime)}`;
+                const dtEnd = `202309${getDayCode(day)}T${formatTime(time.endtime)}`;
+
+                icsData += "BEGIN:VEVENT\r\n";
+                icsData += `UID:${uid}\r\n`;
+                icsData += `SUMMARY:${summary}\r\n`;
+                icsData += `DTSTART:${dtStart}\r\n`;
+                icsData += `DTEND:${dtEnd}\r\n`;
+                icsData += "RRULE:FREQ=WEEKLY;UNTIL=20231231T235959Z\r\n";
+                icsData += "END:VEVENT\r\n";
+            });
+        });
+    });
+
+    icsData += "END:VCALENDAR\r\n";
+    return icsData;
+}
+
+function getDayCode(day) {
+    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    return (days.indexOf(day) + 1).toString().padStart(2, '0');
+}
+
+function formatTime(time) {
+    return time.replace(/(\d+):(\d+)([APM]+)$/, function (match, hh, mm, ampm) {
+        if (ampm === "PM" && hh !== "12") {
+            hh = (parseInt(hh) + 12).toString();
+        }
+        return `${hh}${mm}00`;
+    });
 }
 
 function downloadICS(content) {
