6ee6b7c6e19ebd44106267ba9adee0e8247e97c2
BarelyAnXer <christiangazzingan38@gmail.com>
2023-09-03 20:35:06 +0800

    finished parsing the data

--- contentScript.js
+++ contentScript.js
@@ -9,17 +9,37 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     for (let i = 0; i < rows.length; i++) {
         let data = rows[i].getElementsByTagName('td')
 
+
+        const parsedDates = data[3]?.textContent.trim().match(new RegExp(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].join('|'), 'g'))
+        const parsedRooms = data[5]?.textContent.trim().match(/.{3}/g)
+
+
+        let pairs = data[4]?.textContent.trim().match(/(\d{2}:\d{2}[APM]{2} - \d{2}:\d{2}[APM]{2})/g);
+        let parsedTimes = []
+        
+        if (!pairs) {
+            console.log("No time ranges found.");
+        } else {
+            parsedTimes = pairs.map((timeStr) => {
+                const [start, end] = timeStr.split(" - ");
+                return { starttime: start, endtime: end };
+            });
+        }
+
         const entry = {
             subjectCode: data[0].textContent.trim(),
             subjectName: data[1].textContent.trim(),
             section: data[2]?.textContent.trim(),
-            dates: data[3]?.textContent.trim(),
-            times: data[4]?.textContent.trim(),
-            rooms: data[5]?.textContent.trim(),
+            dates: parsedDates,
+            times: parsedTimes,
+            rooms: parsedRooms,
             professor: data[6]?.textContent.trim(),
         }
         scheduleEntries.push(entry);
     }
+
+    scheduleEntries.pop()
+
     chrome.runtime.sendMessage({
         type: "receiveschedule",
         schedule: scheduleEntries
--- popup.js
+++ popup.js
@@ -15,6 +15,43 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     const alertBox = document.querySelector("#display-box");
     let newTag = document.querySelector(".display-text");
 
+    console.log(request.schedule)
+
+    // const output = separateSchedules(request.schedule);
+    // const iCalData = jsonToICal(output);
+
+    let iCalData = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Your Organization//Your Website//EN
+CALSCALE:GREGORIAN
+METHOD:PUBLISH`
+
+    // BEGIN:VEVENT
+    // UID:9876543210
+    // DTSTAMP:20230903T120000Z
+    // DTSTART:20230903T160000Z
+    // DTEND:20230903T180000Z
+    // SUMMARY:Random Event
+    // DESCRIPTION:This is a randomly generated event description.
+    // LOCATION:Virtual Meeting
+    // ORGANIZER:John Doe
+    // ATTENDEE:Jane Doe
+    // END:VEVENT
+    // END:VCALENDAR
+
+//     iCalData = iCalData + `BEGIN:VEVENT
+// UID:${uid}
+// SUMMARY:${event.subject}
+// DTSTART:${formatICalDate(startDate)}
+// DTEND:${formatICalDate(endDate)}
+// RRULE:FREQ=WEEKLY;UNTIL=${formatICalDate(endRecurringDate)}
+// END:VEVENT\n`;
+
+
+    iCalData += 'END:VCALENDAR';
+
+    downloadICS(iCalData, "schedule");
+
     if (!newTag) {
         const displayText = document.createElement("p");
         displayText.className = "display-text";
@@ -38,3 +75,27 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     alertBox.appendChild(newTag);
     // }
 });
+
+function scheduleToEvents(schedule) {
+
+}
+
+function constructICS() {
+
+}
+
+function downloadICS(content) {
+    const filename = "schedule"
+    console.log("asdk")
+    const blob = new Blob([content], { type: 'text/calendar;charset=utf-8' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = filename + '.ics';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
